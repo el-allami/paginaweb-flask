3dd8cfc7a174bd2e16a63a70c8d1210ded9999d0
el-allami <elallami.ali.nasr@iisgalvanimi.edu.it>
2022-04-28 09:53:29 +0000

    12

--- paginaweb.py
+++ paginaweb.py
@@ -5,9 +5,29 @@ app = Flask(__name__)
 def HomeP():
     return render_template("pagina1.html")
 
-@app.route('/back', methods=['GET'])
-def HomeP():
+@app.route('/HOME', methods=['GET'])
+def Home():
     return render_template("pagina1.html")
+
+@app.route('/come nasce lo skate1', methods=['GET'])
+def comenasceloskate1():
+    return render_template("come nasce lo skate1.html")
+
+@app.route('/come scegliere un setup', methods=['GET'])
+def comescegliereunsetup():
+    return render_template("come scegliere un setup.html")
+
+@app.route('/come scegliere una tavola1', methods=['GET'])
+def comescegliereunatavola1():
+    return render_template("come scegliere una tavola1.html")
+
+@app.route('/come scegliere dei truck', methods=['GET'])
+def comesceglieredeitruck():
+    return render_template("come scegliere dei truck.html")
+
+@app.route('/come scegliere delle ruote', methods=['GET'])
+def comesceglieredelleruote():
+    return render_template("come scegliere delle ruote.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
