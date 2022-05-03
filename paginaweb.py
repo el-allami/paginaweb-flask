0775c0e982b2fb72c7aec4d5f28d3549442e6e72
el-allami <elallami.ali.nasr@iisgalvanimi.edu.it>
2022-05-03 11:29:05 +0000

    ES

--- paginaweb.py
+++ paginaweb.py
@@ -29,5 +29,68 @@ def comesceglieredeitruck():
 def comesceglieredelleruote():
     return render_template("come scegliere delle ruote.html")
 
+@app.route('/pagina12', methods=['GET'])
+def pagina12():
+    return render_template("pagina12.html")
+
+@app.route('/maglietta uomo nike sb', methods=['GET'])
+def magliettauomonikesb():
+    return render_template("maglietta uomo nike sb.html")
+
+@app.route('/magliette uomo adida-skateboard', methods=['GET'])
+def maglietteuomoadidaskateboard():
+    return render_template("magliette uomo adida-skateboard.html")
+
+@app.route('/magliette uomo anti-hero', methods=['GET'])
+def maglietteuomoantihero():
+    return render_template("magliette uomo anti-hero.html")
+ 
+@app.route('/magliette uomo baker', methods=['GET'])
+def maglietteuomobaker():
+    return render_template("magliette uomo baker.html")
+
+@app.route('/magliette uomo thrasher', methods=['GET'])
+def maglietteuomothrasher():
+    return render_template("magliette uomo thrasher.html")
+
+@app.route('/pagina8', methods=['GET'])
+def pagina8():
+    return render_template("pagina8.html")
+
+@app.route('/pagina9', methods=['GET'])
+def pagina9():
+    return render_template("pagina9.html")
+
+@app.route('/pagina2', methods=['GET'])
+def pagina2():
+    return render_template("pagina2.html")
+
+@app.route('/viti ace', methods=['GET'])
+def vitiace():
+    return render_template("viti ace.html")
+
+@app.route('/viti bones', methods=['GET'])
+def vitibones():
+    return render_template("viti bones.html")
+
+@app.route('/viti independent', methods=['GET'])
+def vitiindependent():
+    return render_template("viti independent.html")
+
+@app.route('/viti mino-logo', methods=['GET'])
+def vitiminologo():
+    return render_template("viti mino-logo.html")
+
+@app.route('/grip grizzly', methods=['GET'])
+def gripgrizzly():
+    return render_template("grip grizzly.html")
+
+@app.route('/grip mob', methods=['GET'])
+def gripmob():
+    return render_template("grip mob.html")
+
+@app.route('/grip toy machine', methods=['GET'])
+def griptoymachine():
+    return render_template("grip toy machine.html")
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
